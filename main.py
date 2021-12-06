ef6c142b88a3a16264fdba75d85b008a0cb41ce5
mohitrathor98 <mohitrathor8@gmail.com>
2021-12-06 21:02:43 +0530

    scoreboard

--- main.py
+++ main.py
@@ -13,18 +13,6 @@ screen.bgcolor("black")
 screen.title("Ping-Pong")
 screen.tracer(0)
 
-'''
-TODO:
-
-1) Create and move a paddle == done
-2) Create another paddle == done
-3) create the ball and make it move == done
-4) Detect collision with top & bottom walls and bounce == done
-5) Detect collision with paddle == done
-6) Detect when paddle misses == done
-7) Keep score
-'''
-
 # paddle creation and move
 right_paddle = Paddle((360, 0))
 left_paddle = Paddle((-360, 0))
@@ -43,7 +31,7 @@ screen.onkey(left_paddle.down, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
     
     # detect collision with top or bottom wall
@@ -54,10 +42,18 @@ while game_is_on:
     # detect collision with paddle
     if (ball.distance(right_paddle) < 50 and ball.xcor() > 320) or (ball.distance(left_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_x()
+        ball.move_speed *= 0.9 # increases ball speed
         
     # detect paddle miss
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380: 
+        ball.goto(0,0)
+        ball.bounce_x()
+        scoreboard.l_update()
+        ball.move_speed = 0.1
+        
+    if ball.xcor() < -380:
         ball.goto(0,0)
         ball.bounce_x() 
+        scoreboard.r_update()
 
 screen.exitonclick()
